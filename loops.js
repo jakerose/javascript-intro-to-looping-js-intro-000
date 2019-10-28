60d829b34d3a8da4a5dc54f312873295960da291
Jake Rose <jake1020@mac.com>
2019-10-28 23:08:10 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,20 @@
+function forLoop(array) {
+  for (let i = 0; i < 25; i++) {
+    if (i==1) {
+      return "I am 1 strange loop."
+    }else{
+      return "I am ${} strange loops."
+    }
+}
+
+function whileLoop(n) {
+  let countdown = 100;
+
+  while (countdown > 0) {
+    console.log(--countdown);
+  }
+}
+
+function doWhileLoop(num) {
+
+}
